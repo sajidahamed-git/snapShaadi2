25cfa0536c829511ec64f87901cfe59516479635
Sajid <sajidahamed2001@gmail.com>
2025-06-04 01:29:21 +0530

    can open camera

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,60 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 
-
+import { CameraView, useCameraPermissions } from "expo-camera";
 
 import { Image } from "expo-image";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 
-import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { FontAwesome } from "@expo/vector-icons";
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
+  const [cameraPermission, requestCameraPermission] = useCameraPermissions();
+  const [isCameraOpen, setIsCameraOpen] = useState(false);
+
+  const handleCameraPress = async () => {
+    if (cameraPermission?.granted) {
+      setIsCameraOpen(true);
+    } else {
+      const permission = await requestCameraPermission();
+      if (permission.granted) {
+        setIsCameraOpen(true);
       }
-    >
+    }
+  };
+
+  if (isCameraOpen) {
+    return (
+      <View style={styles.container}>
+        <CameraView style={styles.camera} facing="back">
+          <View style={styles.buttonContainer}></View>
+        </CameraView>
+      </View>
+    );
+  }
+  return (
+    // If camera permission is granted, show the camera view
+    <View style={styles.container}>
+      <Image
+        source={require("@/assets/images/partial-react-logo.png")}
+        style={styles.reactLogo}
+      />
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Snap Shaadi</ThemedText>
       </ThemedView>
       <ThemedView style={styles.buttonContainer}>
-        <FontAwesome.Button name="camera">Open Camera</FontAwesome.Button>
-        <FontAwesome.Button name="image">Open Gallery</FontAwesome.Button>
+        <FontAwesome.Button
+          name="camera"
+          style={styles.button}
+          onPress={handleCameraPress}
+        >
+          Open Camera
+        </FontAwesome.Button>
+        <FontAwesome.Button name="image" style={styles.button}>
+          Open Gallery
+        </FontAwesome.Button>
       </ThemedView>
-    </ParallaxScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  buttonContainer: {
+  container: {
+    flex: 1,
     display: "flex",
     flexDirection: "column",
-    gap: 8,
-    alignItems: "center",
-  },
-  titleContainer: {
-    minHeight: 200,
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-    padding: 16,
-    alignSelf: "center",
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+    backgroundColor: "transparent",
   },
   reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+    minHeight: "50%",
+    width: "100%",
+  },
+  titleContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 16,
+  },
+  buttonContainer: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    gap: 8,
+    alignItems: "center",
+  },
+  button: {
+    paddingVertical: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  camera: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
   },
 });
